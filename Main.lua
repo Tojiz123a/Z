a5f6d002aa67255d2ecd9f64a70413740da37f9d
Tojiz123a <althaffakhri777@gmail.com>
2024-09-10 10:36:38 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,16 @@
+repeat
+    task.wait()
+until game.GameId ~= nil
+repeat
+    task.wait()
+until game:IsLoaded()
+
+if getgenv().loaded then
+    return
+end
+getgenv().loaded = true
+
+universeid = game.GameId
+
+setclipboard("https://discord.gg/px4Axh7BA9")
 
