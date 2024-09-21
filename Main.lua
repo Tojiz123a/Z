ebd62fd3d869a73ccd8c998ee19e126363d2dc4b
Tojiz123a <althaffakhri777@gmail.com>
2024-09-21 20:02:42 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -17,3 +17,7 @@ setclipboard("https://discord.gg/tYDH2szZmx")
 if universeid == 8737899170 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Tojiz123a/Z/refs/heads/main/ps99inju.lua", true))()
 end
+
+if universeid == 142823291 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tojiz123a/Z/refs/heads/main/mm2inju.lua", true))()
+end
